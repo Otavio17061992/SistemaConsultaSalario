8085c29a58b25f527f47e8a08441b7136a2812d5
João Almeida <joaoalmeida48@outlook.com>
2022-12-05 20:53:08 -0300

    Primeira versao

---
+++ Course/Entities/HourContract.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hours { get; set; }
+
+        // contrutor padrao
+        public HourContract()
+        {
+
+        }
+
+        // construtor com argumentos
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
